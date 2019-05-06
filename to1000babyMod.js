523ef1604fe406bb0c2cdf55c5a986fc84bf56c5
lodlodlon <50263837+lodlodlon@users.noreply.github.com>
2019-05-06 20:44:14 +1000

    Update to1000babyMod.js

--- to1000babyMod.js
+++ to1000babyMod.js
@@ -7,15 +7,19 @@ to1000baby_Mod.launch = function(){
 	to1000baby_Mod.lasttick = 0;
 	to1000baby_Mod.tickdelay = 500;
 	to1000baby_Mod.buyArray = [
-		function() {Game.storeBuyAll()},
 		function() {Game.Objects["Cursor"].buy(100)},
 		function() {Game.Objects["Grandma"].buy(100)},
+		function() {Game.storeBuyAll()},
 		function() {Game.Objects["Prism"].buy(100)},
 		function() {Game.Objects["Fractal engine"].buy(100)}
 	]
 	to1000baby_Mod.buyIterator = 0;
-	console.log("loaded");
+	console.log("to1000baby_Mod loaded");
+	if (Game.Has('Inspired checklist')){
 	Game.customLogic.push(to1000baby_Mod.Update);
+	} else {
+		console.log("YOU ARE COOKIE BABY, WAIT TO GET 'Inspired checklist' AND TRY THIS AGAIN");
+	}
 	to1000baby_Mod.isLoaded = 1;
 }
 
@@ -24,10 +28,10 @@ to1000baby_Mod.RealLogic = function(){
 	if (Game.OnAscend) { //LEGACY SCREEN
 	
 		if(Game.ReincarnateTimer>0) { //reincarnate
-	console.log("Waiting to reincarnate");
+	//console.log("Waiting to reincarnate");
 			
 		} else { //wait to reincarnate
-	console.log("Reincarnating");
+	//console.log("Reincarnating");
 			
 Game.Reincarnate(1);
 		}
@@ -36,10 +40,10 @@ Game.Reincarnate(1);
 		
 	
 		if(Game.AscendTimer>0) {//wait to ascend
-	console.log("Waiting to ascend");
+	//console.log("Waiting to ascend");
 			
 		} else {//grind those cookies
-	console.log("Buying");
+	//console.log("Buying");
 		to1000baby_Mod.buyArray[to1000baby_Mod.buyIterator]();
 		to1000baby_Mod.buyIterator++;
 		to1000baby_Mod.buyIterator %= 5;
